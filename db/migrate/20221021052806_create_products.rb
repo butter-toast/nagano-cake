6a8fff9c6a317bd0ca511c982ec3e66141b497b2
EC2 Default User <73773480+kyyysnr@users.noreply.github.com>
2022-10-21 05:43:25 +0000

    商品モデルと商品ジャンルモデルの作成

---
+++ db/migrate/20221021052806_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.integer :product_genre_id, null: false
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.integer :no_tax_price, null: false
+      t.boolean :is_sale_status, null: false, default:false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_041947) do
+ActiveRecord::Schema.define(version: 2022_10_21_053944) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,22 @@ ActiveRecord::Schema.define(version: 2022_10_21_041947) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "product_genre_id", null: false
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "no_tax_price", null: false
+    t.boolean "is_sale_status", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
