b6127c5de0538682ee6e4cab22b1781df2262d91
EC2 Default User <73773480+kyyysnr@users.noreply.github.com>
2022-10-22 14:16:06 +0000

    ProductsController更新

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,19 +9,30 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    redirect_to admin_products_
+    redirect_to admin_product_path(@product)
   end
 
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product)
+    else
+      render :edit
+    end
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:profile_image,:name,:introduction,:no_tax_price,:is_sale_status,:product_genre_id)
+    params.require(:product).permit(:image,:name,:introduction,:no_tax_price,:is_sale_status,:product_genre_id)
   end
 end
