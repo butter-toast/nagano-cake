865d113fd10623d93835e4f5afa4ce0bb4bf6791
Ruka2222 <111208324+Akiri2222@users.noreply.github.com>
2022-10-24 13:10:44 +0000

    cart_item,shipping

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,18 @@
-class CartItemsController < ApplicationController
+class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
 
   def index
     @cart_items = current_customer.cart_items.all
   end
 
   def create
-    @cart_item = current_customer.cart_items.new(cart_item_params)
-    if current_customer.cart_items.find_by(product_id: params[:cart_item][:item_id]).present?
-      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    if current_customer.cart_items.find_by(product_id: params[:cart_item][:product_id]).present?
+      cart_item = current_customer.cart_items.find_by(product_id: params[:cart_item][:product_id])
       cart_item.quantity += params[:cart_item][:quantity].to_i
-      cart_item.save
-      redirect_to cart_item_path
+      cart_item.update(quantity: cart_item.quantity)
+      redirect_to cart_items_path
     elsif
+      @cart_item = current_customer.cart_items.new(cart_item_params)
       @cart_item.save
       @cart_items = current_customer.cart_items.all
       render 'index'
@@ -21,6 +22,9 @@ class CartItemsController < ApplicationController
   end
 
   def update
+    cart_item = current_customer.cart_items.find_by(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
