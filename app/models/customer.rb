139c0bdd932cbc75200bc1162ffba6f64e9571af
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-10-25 07:32:41 +0000

    Fix 配送先一覧

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
    
   has_many :cart_items
+  has_many :shippings
 
   validates :last_name, presence:true
   validates :first_name, presence:true
